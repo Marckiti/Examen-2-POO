2fd878b996a75f0daac16caad221f4d85298e608
Marckiti <arayaarpimarcos@gmail.com>
2026-01-17 10:53:53 -0500

    estructura y modelo Producto

---
+++ src/main/java/com/example/Examen2/model/Producto.java
@@ -0,0 +1,23 @@
+package com.example.Examen2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Double precio;
+    private Integer stock;
+}
+
