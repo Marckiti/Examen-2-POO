7bd2793a68b9145b611fd8a65584a3c51fad1257
Said_QuintoNevarez <quintonevarezariel@gmail.com>
2026-01-18 21:20:19 -0500

    hola, le creo el ProductoService usando la interfaz que creaste tu y aplicar los metodos necesarios para el Service

---
+++ src/main/java/com/example/Examen2/service/ProductoServiceImp.java
@@ -0,0 +1,35 @@
+package com.example.Examen2.service;
+
+
+import com.example.Examen2.model.Producto;
+import com.example.Examen2.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductoServiceImp implements IProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public List<Producto> listarProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public Producto buscarPorId(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Producto guardar(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        productoRepository.deleteById(id);
+    }
+}
