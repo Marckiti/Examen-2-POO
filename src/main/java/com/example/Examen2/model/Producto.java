fdd4b71f10a96ee72556a29409bf8b0c0fe732c2
Said_QuintoNevarez <quintonevarezariel@gmail.com>
2026-01-21 18:52:39 -0500

    agrego un atributo mas porque son 5

--- src/main/java/com/example/Examen2/model/Producto.java
+++ src/main/java/com/example/Examen2/model/Producto.java
@@ -22,12 +22,20 @@ public class Producto {
     private Long id;
 
     @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     private String nombre;
+
+    @NotBlank(message = "La descripcion es obligatoria")
+    @Size (min = 5, max = 100, message = "La descripcion debe estar entre 5 y 100 caracteres")
+    private String descripcion;
+
 
     @Min(value = 0, message = "El precio no puede ser menor a 0")
     @NotNull(message = "El precio es obligatorio")
     private Double precio;
 
+    @NotNull(message = "El stock es obligatorio")
     @Min(value = 0, message = "El stock no puede ser negativo")
     private Integer stock;
+
 }
