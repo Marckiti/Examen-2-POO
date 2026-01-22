b233627a0a8d70e436e96458a7fec935e5ef4753
Said_QuintoNevarez <quintonevarezariel@gmail.com>
2026-01-21 19:42:27 -0500

    Se cambia la mayoria de metodos para obtener una respuesta mejor en las APIS y hacer uso de las validaciones

--- src/main/java/com/example/Examen2/controller/ProductoController.java
+++ src/main/java/com/example/Examen2/controller/ProductoController.java
@@ -18,36 +18,33 @@ public class ProductoController {
     private IProductoService productoService;
 
     @GetMapping
-    public List<Producto> listar() {
-        return productoService.listarProductos();
+    public ResponseEntity<List<Producto>> listar() {
+        return ResponseEntity.ok(productoService.listarProductos());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Producto> obtenerPorId(@PathVariable Long id) {
-        Producto p = productoService.buscarPorId(id);
-        return (p != null) ? ResponseEntity.ok(p) : ResponseEntity.notFound().build();
+        return ResponseEntity.ok(productoService.buscarPorId(id));
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Producto crear(@Valid @RequestBody Producto producto) {
-        return productoService.guardar(producto);
+    public ResponseEntity<Producto> crear(@Valid @RequestBody Producto producto) {
+        Producto crear = productoService.guardar(producto);
+        return new ResponseEntity<>(crear,HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public Producto actualizar(@RequestBody Producto producto, @PathVariable Long id) {
+    public ResponseEntity<Producto> actualizar(@Valid @RequestBody Producto producto, @PathVariable Long id) {
         Producto productoActual = productoService.buscarPorId(id);
-        if (productoActual != null) {
-            productoActual.setNombre(producto.getNombre());
-            productoActual.setPrecio(producto.getPrecio());
-            productoActual.setStock(producto.getStock());
-            return productoService.guardar(productoActual);
-        }
-        return null;
+        productoActual.setNombre(producto.getNombre());
+        productoActual.setPrecio(producto.getPrecio());
+        productoActual.setStock(producto.getStock());
+        return ResponseEntity.ok(productoService.guardar(productoActual));
     }
 
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         productoService.eliminar(id);
+        return ResponseEntity.noContent().build();
     }
 }
