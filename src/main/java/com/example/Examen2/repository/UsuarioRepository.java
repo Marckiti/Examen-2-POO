8c1a4f940dbb4720e0ac2a439c39c68a40207f89
Said_QuintoNevarez <quintonevarezariel@gmail.com>
2026-01-24 16:12:13 -0500

    se eliminan metodo para crear usuarios

--- src/main/java/com/example/Examen2/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Examen2.repository;
-
-import com.example.Examen2.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
