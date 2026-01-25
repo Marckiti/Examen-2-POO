315a1d80a8a17a3dfe9a6588c062f1c3a6cf7ba4
Marckiti <arayaarpimarcos@gmail.com>
2026-01-25 17:07:44 -0500

    Update CrossOrigin annotation to allow multiple origins

--- src/main/java/com/example/Examen2/controller/ProductoController.java
+++ src/main/java/com/example/Examen2/controller/ProductoController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://la-tiendita-api.netlify.app/"})
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
